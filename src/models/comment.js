32bd2ddf1a55ebfde5ad9897f54e9eae92ca7b7d
Ali <alidrl26@gmail.com>
2023-12-17 21:15:02 +0200

    first commit

---
+++ src/models/comment.js
@@ -0,0 +1,34 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const { Schema, model } = require("mongoose");
+/* ------------------------------------------------------- *
+
+/* ------------------------------------------------------- */
+// Comment Model:
+
+const CommentSchema = new Schema(
+  {
+    user: { },
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "Blog",
+      required: true,
+    },
+  },
+  { collection: "comments", timestamps: true }
+);
+
+CommentSchema.pre("init", function (data) {
+  data.id = data._id;
+});
+
+/* ------------------------------------------------------- */
+module.exports = model("Comment", CommentSchema);
